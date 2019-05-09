4f3695eafba820ce8df6875a99ab5a72bf18f464
Florisuga fusca <pedrovrima@gmail.com>
2019-05-09 16:47:34 -0300

    OAMa name larger

--- react_spa/src/HomeSects.js
+++ react_spa/src/HomeSects.js
@@ -9,12 +9,15 @@ export function DeskEntry  (sect) {
 return(
 <div>
 <img src={sect.sect.fig}/>
-<h1>{sect.sect.title}</h1>
+<span><h1>{sect.sect.pretitle}</h1>
+<h2>{sect.sect.oamtit}</h2>
+</span>
 <span>{sect.sect.text}</span>
 </div>
 )};
 
 export var sessoes= {id:"entry" ,
-title: "Bem-vindo ao portal do Observatório de Aves da Mantiqueira",
-fig:"/figs/logoescritosemfundo.png",
+pretitle: "Bem-vindo ao portal do",
+oamtit: "Observatório de Aves da Mantiqueira",
+fig:"/figs/tt.png",
 text: "O Observatório de Aves da Mantiqueira é uma organização científica independente voltada para a pesquisa e conservação de aves na região da Serra da Mantiqueira"};
