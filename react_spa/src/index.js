1f0b142a563858908ea1e0e8b590943aa6762e46
Florisuga fusca <pedrovrima@gmail.com>
2019-05-15 18:47:15 -0300

    add favicon

--- react_spa/src/index.js
+++ react_spa/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
+import Favicon from 'react-favicon';
+
 
 import "./index.css";
 ReactDOM.render(
-  <Main/>,
+  <div>
+ <Favicon url="/figs/thebird.png"/>
+  <Main/>
+  </div>,
   document.getElementById("root")
 );
